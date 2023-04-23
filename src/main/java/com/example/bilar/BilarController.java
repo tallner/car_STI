b2e620e9870dbc8e9fd3cf4a2c0e047400b2d73a
tallner <ctallner@hotmail.com>
2023-04-23 23:01:01 +0200

    test

--- src/main/java/com/example/bilar/BilarController.java
+++ src/main/java/com/example/bilar/BilarController.java
@@ -64,16 +64,16 @@ public class BilarController {
 /* */
 
 
-    @GetMapping(value="/car")
+    @GetMapping(value="/car", produces = MediaType.TEXT_PLAIN_VALUE)
     @CrossOrigin()
-    public List<Car> getall() {
+    public ResponseEntity<List<Car>> getall() {
         var l = new ArrayList<Car>();
 
         l.add(new Car("Volvo", "XC70", "Blå", 2008,4));
         l.add(new Car("Volvo", "XC60", "Svart", 2015,4));
         l.add(new Car("Volvo", "240", "Röd", 1985,6));
         l.add(new Car("Renault", "Megane", "Svart", 2018,4));
-        return l;
+        return ResponseEntity.ok(l);
 
     }
     
