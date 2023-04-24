0f3e6e9de10c939ab90e3975725a274d55db2b39
tallner <ctallner@hotmail.com>
2023-04-24 08:42:42 +0200

    test

--- src/main/java/com/example/bilar/Bilar.java
+++ src/main/java/com/example/bilar/Bilar.java
@@ -7,11 +7,11 @@ import javax.persistence.Id;
 
 //fkn
 
-@Entity
+//@Entity
 public class Bilar {
     
-    @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    //@Id
+    //@GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
     private static Integer num = 0;
 
